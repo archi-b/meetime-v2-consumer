b3d031d1e3fcc5ff8fb8fbb176461c0bdbb6031d
Max J. Prass <maxjeison@gmail.com>
2023-10-30 12:22:42 -0300

    Add pagination in 'buscarLead_D1' api

--- meetimeApi.py
+++ meetimeApi.py
@@ -8,7 +8,7 @@ from objects.lead import Lead
 class MeetimeApi(object):
 
     @property
-    def headers(self):
+    def __headers(self):
         return {
             'Accept': 'application/json',
             'Authorization': self.token
@@ -40,26 +40,33 @@ class MeetimeApi(object):
 
         self.dtToday = date.datetime.today().strftime(self.date_format)
         self.dtTodayD1 = (date.datetime.today() - date.timedelta(days=1)).strftime(self.date_format)
-        self.__setUrl(0)
-
-    def __setUrl(self, start=0):
-        self.url = f"{self.host}{self.stage}/leads?limit={self.items_by_request}&" \
-            f"start={start}&" \
-            f"lead_created_after={self.dtTodayD1}&" \
-            f"lead_created_before={self.dtToday}&" \
-            f"lead_updated_after={self.dtTodayD1}&" \
-            f"lead_updated_before={self.dtToday}&" \
-            f"show_deleted={self.show_deleted}"
-
-    def buscarLead_D1(self):
-        arrayLeads = []
-        start = 0
-        response = requests.request("GET", self.url, headers=self.headers, data={})
-        lead_json = json.loads(response.text)
-        leads = Lead(**lead_json)
-        arrayLeads = leads.data
-
-        # TODO: realizar paginacao aqui para obter todos os leads
-        # arrayLeads.extend(leads.data)
         
-        return arrayLeads
+    def buscarLead_D1(self):
+        # Init object
+        if (hasattr(self, "url") == False):
+            self.__arrayLeads = []
+            self.__url = f"{self.host}{self.stage}/leads?limit={self.items_by_request}&" \
+                f"start=0&" \
+                f"lead_created_after={self.dtTodayD1}&" \
+                f"lead_created_before={self.dtToday}&" \
+                f"lead_updated_after={self.dtTodayD1}&" \
+                f"lead_updated_before={self.dtToday}&" \
+                f"show_deleted={self.show_deleted}"
+
+        response = requests.request("GET", self.__url, headers=self.__headers, data={})
+
+        lead = lambda:None
+        lead.__dict__ = json.loads(response.text)
+
+        if (hasattr(lead, "size") is False):
+            lead.size = 0
+        leads = Lead(lead.limit, lead.start, lead.size, lead.next, lead.data, lead.parameters)
+        
+        self.__arrayLeads.extend(leads.data)
+
+        # Pagination
+        while (leads.next is not None):
+            self.__url = f"{self.host}" + lead.next
+            return self.buscarLead_D1()
+
+        return self.__arrayLeads
